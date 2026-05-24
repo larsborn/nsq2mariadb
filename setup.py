ccd34650068db556e71fe8a1146cabf35fd2f0a8
Lars Wallenborn <lars@wallenborn.net>
2026-05-24 12:26:29 +0200

    chore: bump 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setuptools.setup(
     name="nsq2mariadb",
-    version="0.1.2",
+    version="0.1.3",
     author="Lars Wallenborn",
     description="generic NSQ → MariaDB transporter with per-topic Python mapper classes",
     long_description=long_description,
